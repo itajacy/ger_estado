05d9dd5bca2d07de0dbcd0180efb77d264dc9de1
ita Dev <itajacy@gmail.com>
2025-04-09 21:49:08 -0300

    usando copyWith, e imutabilidade

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 
 
 
@@ -5,4 +6,21 @@ class Person {
   final double height;
   final double weight;
   Person({required this.height, required this.weight});
+
+
+  
+
+  double calcularIMC() {
+    return weight / (height * height);
+  }
+
+  Person copyWith({
+    double? height,
+    double? weight,
+  }) {
+    return Person(
+      height: height ?? this.height,
+      weight: weight ?? this.weight,
+    );
+  }
 }
