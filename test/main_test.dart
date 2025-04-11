b6ad3f66f9f24464cc2ce662cd45a697016e9139
itajacy TI02 <itajacy@bsd.com.br>
2025-04-11 14:54:03 -0300

    usando Cubit, do package Bloc e Streams

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 
 class Product {
   String name;
@@ -45,16 +45,12 @@ class ProductState {
 
 // PROPAGAÇÃO DO ESTADO
 //  Imutabilidade com gerenciamento de estado
-class ProductViewModel extends ValueNotifier<ProductState> {
+class ProductViewModel extends Cubit<ProductState> {
   ProductViewModel() : super(ProductState());
-
-  void emit(ProductState state) {
-    value = state;
-  }
 
   Future<void> fetchProducts({required bool isError}) async {
     // Reset state
-    emit(value.setLoading(true));
+    emit(state.setLoading(true));
 
     // antes de entrar aqui abaixo que vai demorar para exibir a tela
     // simulando uma demora de 2 segundos
@@ -62,14 +58,14 @@ class ProductViewModel extends ValueNotifier<ProductState> {
 
     // tratativa  em caso de erro
     if (isError) {
-      emit(value.setError('Error fetching products'));
+      emit(state.setError('Error fetching products'));
 
       return;
     }
 
     // tratativa  em caso de sucesso
     emit(
-      value.setProducts([
+      state.setProducts([
         Product('Product 1', 10.0),
         Product('Product 2', 20.0),
         Product('Product 3', 30.0),
@@ -83,10 +79,26 @@ class ProductViewModel extends ValueNotifier<ProductState> {
 //! é exibida uma mensagem de loading, e quando o processo terminar
 //! será exibido o resultado, isso usando os listeners
 void main() async {
+  //*
+
+  //   tudo abaixo são só formas diferentes de propagar o estado
+  // porque o gerenciamento  ocorre nas classes
+  //ChangeNotifier
+  //Mobx
+  //Bloc
+  //Cubit
+  //Asp
+  //Getx
+  //Riverpod
+  //Provider
+  //GetIt
+  //Signals */
+
+  //Propagação de estado
   ProductViewModel productViewModel = ProductViewModel();
 
-  productViewModel.addListener(() {
-    final state = productViewModel.value;
+  productViewModel.stream.listen((state) {
+    // final state = productViewModel.state;
     if (state.isLoading) {
       print('Objects Loading....');
     } else if (state.isError) {
@@ -96,5 +108,5 @@ void main() async {
     }
   });
   //testando com isError =  false e true
-  await productViewModel.fetchProducts(isError: false);
+  await productViewModel.fetchProducts(isError: true);
 }
